da638fa20b39b12fcde17b7390a49d6e597e0bf1
Maksim Dvoretskii <kamerok@gmail.com>
2019-06-27 23:35:45 +0300

    Run requests in parallel

--- core/domain/src/main/java/easydone/core/domain/DomainRepository.kt
+++ core/domain/src/main/java/easydone/core/domain/DomainRepository.kt
@@ -3,6 +3,8 @@ package easydone.core.domain
 import easydone.core.auth.AuthInfoHolder
 import easydone.core.domain.model.Task
 import easydone.library.trelloapi.TrelloApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
@@ -13,12 +15,15 @@ class DomainRepository(
 ) {
 
     fun getTasks(isInbox: Boolean): Flow<List<Task>> = flow {
-        val boardId = authInfoHolder.getBoardId()!!
-        val token = authInfoHolder.getToken()!!
-        val lists = trelloApi.lists(boardId, TrelloApi.API_KEY, token)
-        val listId = lists[if (isInbox) 0 else 1].id
-        val cards = trelloApi.cards(boardId, TrelloApi.API_KEY, token).filter { it.idList == listId }
-        emit(cards.map { Task(it.id, it.name) })
+        val filteredCards = coroutineScope {
+            val boardId = authInfoHolder.getBoardId()!!
+            val token = authInfoHolder.getToken()!!
+            val lists = async { trelloApi.lists(boardId, TrelloApi.API_KEY, token) }
+            val cards = async { trelloApi.cards(boardId, TrelloApi.API_KEY, token) }
+            val listId = lists.await()[if (isInbox) 0 else 1].id
+            cards.await().filter { it.idList == listId }
+        }
+        emit(filteredCards.map { Task(it.id, it.name) })
     }
 
 }
