649ce5e7d12e84d30b6b82a060de98d3779b3b9e
Maksim Dvoretskii <kamerok@gmail.com>
2019-06-30 15:01:37 +0300

    Add differ

--- core-ui/taskitem/src/main/java/easydone/coreui/taskitem/TaskAdapter.kt
+++ core-ui/taskitem/src/main/java/easydone/coreui/taskitem/TaskAdapter.kt
@@ -4,6 +4,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_task.*
@@ -13,7 +15,13 @@ class TaskAdapter(
     private val listener: (String) -> Unit
 ) : RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
 
-    private var data = mutableListOf<TaskUiModel>()
+    private val differ = AsyncListDiffer(this, object : DiffUtil.ItemCallback<TaskUiModel>() {
+        override fun areItemsTheSame(oldItem: TaskUiModel, newItem: TaskUiModel): Boolean =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: TaskUiModel, newItem: TaskUiModel): Boolean =
+            oldItem == newItem
+    })
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
         ViewHolder(
@@ -21,15 +29,12 @@ class TaskAdapter(
             listener
         )
 
-    override fun getItemCount(): Int = data.size
+    override fun getItemCount(): Int = differ.currentList.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(data[position])
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+        holder.bind(differ.currentList[position])
 
-    fun setData(data: List<TaskUiModel>) {
-        this.data.clear()
-        this.data.addAll(data)
-        notifyDataSetChanged()
-    }
+    fun setData(data: List<TaskUiModel>) = differ.submitList(data)
 
     class ViewHolder(
         override val containerView: View,
