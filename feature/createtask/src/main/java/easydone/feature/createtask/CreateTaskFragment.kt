d0e46a92fd2d008e0d13b777dc26c11d7e43f0e2
Maksim Dvoretskii <kamerok@gmail.com>
2019-07-04 22:36:46 +0300

    Add ime option

--- feature/createtask/src/main/java/easydone/feature/createtask/CreateTaskFragment.kt
+++ feature/createtask/src/main/java/easydone/feature/createtask/CreateTaskFragment.kt
@@ -1,9 +1,12 @@
 package easydone.feature.createtask
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import easydone.core.domain.DomainRepository
 import easydone.coreui.utils.showKeyboard
@@ -26,15 +29,26 @@ class CreateTaskFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         requireActivity().showKeyboard()
-        createView.setOnClickListener {
-            GlobalScope.launch(Dispatchers.IO) {
-                repository.createTask(
-                    titleView.text.toString(),
-                    descriptionView.text.toString(),
-                    skipInboxView.isChecked
-                )
-                navigator.closeScreen()
+        titleView.setOnEditorActionListener(object : TextView.OnEditorActionListener {
+            override fun onEditorAction(v: TextView, actionId: Int, event: KeyEvent?): Boolean {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    saveTask()
+                    return true
+                }
+                return false
             }
+        })
+        createView.setOnClickListener { saveTask() }
+    }
+
+    private fun saveTask() {
+        GlobalScope.launch(Dispatchers.IO) {
+            repository.createTask(
+                titleView.text.toString(),
+                descriptionView.text.toString(),
+                skipInboxView.isChecked
+            )
+            navigator.closeScreen()
         }
     }
 
