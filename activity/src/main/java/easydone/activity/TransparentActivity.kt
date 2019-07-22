0aca5e51af2ef3ac1c15ea45e7777df93b54b780
Maksim Dvoretskii <kamerok@gmail.com>
2019-07-22 09:44:11 +0300

    Add transparent activity

---
+++ activity/src/main/java/easydone/activity/TransparentActivity.kt
@@ -0,0 +1,14 @@
+package easydone.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class TransparentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transparent)
+    }
+
+}
