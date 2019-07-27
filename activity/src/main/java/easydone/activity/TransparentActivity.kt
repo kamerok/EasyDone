9a40397f97876177f81dc32c48d0c8e8bd2ac8ee
Maksim Dvoretskii <kamerok@gmail.com>
2019-07-27 23:48:14 +0300

    Fix quick add placement

--- activity/src/main/java/easydone/activity/TransparentActivity.kt
+++ activity/src/main/java/easydone/activity/TransparentActivity.kt
@@ -1,6 +1,7 @@
 package easydone.activity
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.kamer.builder.StartFlow
 
@@ -10,6 +11,9 @@ class TransparentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transparent)
+        window.attributes = window.attributes.apply {
+            height = WindowManager.LayoutParams.MATCH_PARENT
+        }
 
         StartFlow.startCreate(this, R.id.containerView)
     }
