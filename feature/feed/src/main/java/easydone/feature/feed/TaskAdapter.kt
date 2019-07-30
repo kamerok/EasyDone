f43fc1772a7973980d415b71d89eeb524beaf0bf
Maksim Dvoretskii <kamerok@gmail.com>
2019-07-30 18:14:09 +0300

    Rewrite adapter to adapter delegaes

--- feature/feed/src/main/java/easydone/feature/feed/TaskAdapter.kt
+++ feature/feed/src/main/java/easydone/feature/feed/TaskAdapter.kt
@@ -1,62 +1,24 @@
 package easydone.feature.feed
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.core.view.isVisible
-import androidx.recyclerview.widget.AsyncListDiffer
+import android.annotation.SuppressLint
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.item_task.*
+import com.hannesdorfmann.adapterdelegates4.AsyncListDifferDelegationAdapter
 
 
-class TaskAdapter(
-    private val listener: (String) -> Unit
-) : RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
+class TaskAdapter(listener: (String) -> Unit) : AsyncListDifferDelegationAdapter<Any>(Callback) {
 
-    private val differ = AsyncListDiffer(this, object : DiffUtil.ItemCallback<TaskUiModel>() {
-        override fun areItemsTheSame(oldItem: TaskUiModel, newItem: TaskUiModel): Boolean =
-            oldItem.id == newItem.id
-
-        override fun areContentsTheSame(oldItem: TaskUiModel, newItem: TaskUiModel): Boolean =
-            oldItem == newItem
-    })
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
-        ViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.item_task,
-                parent,
-                false
-            ),
-            listener
-        )
-
-    override fun getItemCount(): Int = differ.currentList.size
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
-        holder.bind(differ.currentList[position])
-
-    fun setData(data: List<TaskUiModel>) = differ.submitList(data)
-
-    class ViewHolder(
-        override val containerView: View,
-        private val listener: (String) -> Unit
-    ) : RecyclerView.ViewHolder(containerView), LayoutContainer {
-
-        private var item: TaskUiModel? = null
-
-        init {
-            containerView.setOnClickListener { item?.run { listener(this.id) } }
-        }
-
-        fun bind(item: TaskUiModel) {
-            this.item = item
-
-            titleView.text = item.title
-            descriptionView.isVisible = item.hasDescription
-        }
-
+    init {
+        delegatesManager.addDelegate(taskDeletage(listener))
     }
+
+}
+
+object Callback : DiffUtil.ItemCallback<Any>() {
+    override fun areItemsTheSame(oldItem: Any, newItem: Any): Boolean = when {
+        oldItem is TaskUiModel && newItem is TaskUiModel -> oldItem.id == newItem.id
+        else -> oldItem === newItem
+    }
+
+    @SuppressLint("DiffUtilEquals")
+    override fun areContentsTheSame(oldItem: Any, newItem: Any): Boolean = oldItem == newItem
 }
