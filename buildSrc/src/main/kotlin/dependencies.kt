cc974b55e010cbf487082e3f32fb4d53f520defa
Maksim Dvoretskii <kamerok@gmail.com>
2019-08-02 23:53:00 +0300

    Rename dependencies

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -1,87 +1,83 @@
 @file:Suppress("MayBeConstant")
 
 object Config {
-    val minSdk = 21
     val compileSdk = 28
+    val minSdk = 21
     val targetSdk = 28
 }
 
 object Versions {
-    val androidx_appcompat = "1.1.0-rc01"
-    val androidx_core = "1.2.0-alpha02"
-    val androidx_recyclerview = "1.0.0"
-    val androidx_constraintLayout = "1.1.3"
-    val androidx_navigation = "2.1.0-alpha05"
-    val material = "1.1.0-alpha08"
-
-    val adapter_depegates = "4.1.1"
+    val adapterDelegates = "4.1.1"
+    val appCompat = "1.1.0-rc01"
+    val constraintLayout = "1.1.3"
+    val core = "1.2.0-alpha02"
     val coroutines = "1.3.0-RC"
     val crashlytics = "2.10.1"
+    val fabric = "1.30.0"
     val firebase = "17.0.1"
+    val googleServices = "4.3.0"
+    val gradleAndroid = "3.5.0-rc02"
+    val gradleVersions = "0.21.0"
+    val junit = "4.12"
+    val kotlin = "1.3.41"
     val markwon = "4.0.2"
+    val material = "1.1.0-alpha08"
+    val recyclerView = "1.0.0"
     val retrofit = "2.6.0"
-    val retrofit_logging = "4.0.1"
-    val rxjava = "2.2.10"
+    val retrofitLogging = "4.0.1"
+    val rxJava = "2.2.10"
+    val sqlDelight = "1.1.4"
     val stetho = "1.5.1"
     val timber = "5.0.0-SNAPSHOT"
-
-    val junit = "4.12"
-
-    val kotlin = "1.3.41"
-    val fabric = "1.30.0"
-    val gradleandroid = "3.5.0-rc02"
-    val gradleversions = "0.21.0"
-    val sqldelight = "1.1.4"
-    val google_services = "4.3.0"
-
 }
 
 object Plugins {
     val fabric = "io.fabric.tools:gradle:${Versions.fabric}"
-    val google_services = "com.google.gms:google-services:${Versions.google_services}"
-    val gradle_android = "com.android.tools.build:gradle:${Versions.gradleandroid}"
-    val gradleversions = "com.github.ben-manes:gradle-versions-plugin:${Versions.gradleversions}"
+    val googleServices = "com.google.gms:google-services:${Versions.googleServices}"
+    val gradleAndroid = "com.android.tools.build:gradle:${Versions.gradleAndroid}"
+    val gradleVersions = "com.github.ben-manes:gradle-versions-plugin:${Versions.gradleVersions}"
     val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
-    val safeArgs =
-        "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.androidx_navigation}"
-    val sqldelight = "com.squareup.sqldelight:gradle-plugin:${Versions.sqldelight}"
+    val sqlDelight = "com.squareup.sqldelight:gradle-plugin:${Versions.sqlDelight}"
 }
 
-object Deps {
-    val androidx_appcompat = "androidx.appcompat:appcompat:${Versions.androidx_appcompat}"
-    val androidx_core = "androidx.core:core-ktx:${Versions.androidx_core}"
-    val androidx_constraintlayout =
-        "androidx.constraintlayout:constraintlayout:${Versions.androidx_constraintLayout}"
-    val androidx_fragment = "androidx.fragment:fragment-ktx:${Versions.androidx_appcompat}"
-    val androidx_material = "com.google.android.material:material:${Versions.material}"
-    val androidx_navigation_fragment =
-        "androidx.navigation:navigation-fragment-ktx:${Versions.androidx_navigation}"
-    val androidx_navigation_ui =
-        "androidx.navigation:navigation-ui-ktx:${Versions.androidx_navigation}"
-    val androidx_recyclerview =
-        "androidx.recyclerview:recyclerview:${Versions.androidx_recyclerview}"
+object Android {
+    val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    val core = "androidx.core:core-ktx:${Versions.core}"
+    val fragment = "androidx.fragment:fragment-ktx:${Versions.appCompat}"
+    val material = "com.google.android.material:material:${Versions.material}"
+    val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerView}"
+}
 
-    val kotilnStdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
-    val adapter_depelegates = "com.hannesdorfmann:adapterdelegates4:${Versions.adapter_depegates}"
-    val adapter_depelegates_dsl =
-        "com.hannesdorfmann:adapterdelegates4-kotlin-dsl-layoutcontainer:${Versions.adapter_depegates}"
-    val coroutines_core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
-    val coroutines_andorid =
-        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
-    val coroutines_rx2 = "org.jetbrains.kotlinx:kotlinx-coroutines-rx2:${Versions.coroutines}"
+object Kotlin {
+    val coroutinesAndorid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
+    val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
+    val coroutinesRx = "org.jetbrains.kotlinx:kotlinx-coroutines-rx2:${Versions.coroutines}"
+    val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
+}
+
+object Tools {
     val crashlytics = "com.crashlytics.sdk.android:crashlytics:${Versions.crashlytics}"
     val firebase = "com.google.firebase:firebase-core:${Versions.firebase}"
-    val markwon = "io.noties.markwon:core:${Versions.markwon}"
-    val markwon_linkify = "io.noties.markwon:linkify:${Versions.markwon}"
-    val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
-    val retrofit_converter_gson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
-    val retrofit_logging = "com.squareup.okhttp3:logging-interceptor:${Versions.retrofit_logging}"
-    val rxjava = "io.reactivex.rxjava2:rxjava:${Versions.rxjava}"
-    val sqldelight_android = "com.squareup.sqldelight:android-driver:${Versions.sqldelight}"
-    val sqldelight_rxjava = "com.squareup.sqldelight:rxjava2-extensions:${Versions.sqldelight}"
     val stetho = "com.facebook.stetho:stetho:${Versions.stetho}"
-    val timber = "com.jakewharton.timber:timber-android:${Versions.timber}"
-    val timber_jdk = "com.jakewharton.timber:timber-jdk:${Versions.timber}"
+}
 
+object Libraries {
+    val adapterDepelegates = "com.hannesdorfmann:adapterdelegates4:${Versions.adapterDelegates}"
+    val adapterDepelegatesDsl = "com.hannesdorfmann:adapterdelegates4-kotlin-dsl-layoutcontainer:${Versions.adapterDelegates}"
+    val markwon = "io.noties.markwon:core:${Versions.markwon}"
+    val markwonLinkify = "io.noties.markwon:linkify:${Versions.markwon}"
+    val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    val retrofitConverterGson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+    val retrofitLogging = "com.squareup.okhttp3:logging-interceptor:${Versions.retrofitLogging}"
+    val rxJava = "io.reactivex.rxjava2:rxjava:${Versions.rxJava}"
+    val sqlDelightAndroid = "com.squareup.sqldelight:android-driver:${Versions.sqlDelight}"
+    val sqlDelightRxJava = "com.squareup.sqldelight:rxjava2-extensions:${Versions.sqlDelight}"
+    val timberAndroid = "com.jakewharton.timber:timber-android:${Versions.timber}"
+    val timberJdk = "com.jakewharton.timber:timber-jdk:${Versions.timber}"
+    val timber = arrayOf(timberAndroid, timberJdk)
+}
+
+object Test {
     val junit = "junit:junit:${Versions.junit}"
 }
