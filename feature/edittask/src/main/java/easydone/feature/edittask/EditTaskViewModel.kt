650713bcf32020a3d274b2a3487ea2eca61d15e6
Maksim Dvoretskii <kamerok@gmail.com>
2021-11-07 02:47:00 +0300

    Allow to edit task

--- feature/edittask/src/main/java/easydone/feature/edittask/EditTaskViewModel.kt
+++ feature/edittask/src/main/java/easydone/feature/edittask/EditTaskViewModel.kt
@@ -4,10 +4,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import easydone.core.domain.DomainRepository
 import easydone.core.domain.model.Task
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.scan
 import kotlinx.coroutines.flow.stateIn
 import java.time.LocalDate
 import java.time.Period
@@ -19,15 +23,33 @@ internal class EditTaskViewModel(
     repository: DomainRepository
 ) : ViewModel() {
 
+    private val actionChannel: Channel<Action> = Channel(capacity = Channel.UNLIMITED)
+
     val state: StateFlow<State> = flow { emit(repository.getTask(id)) }
-        .map { task ->
-            State(
-                type = task.type.format(task.dueDate),
-                title = task.title,
-                description = task.description,
-                isUrgent = task.markers.isUrgent,
-                isImportant = task.markers.isImportant
-            )
+        .flatMapConcat { originalTask ->
+            actionChannel
+                .consumeAsFlow()
+                .scan(originalTask) { task, action ->
+                    when (action) {
+                        is Action.TitleChange -> task.copy(title = action.title)
+                        is Action.DescriptionChange -> task.copy(description = action.description)
+                        is Action.UrgentClick -> task.copy(
+                            markers = task.markers.copy(isUrgent = !task.markers.isUrgent)
+                        )
+                        is Action.ImportantClick -> task.copy(
+                            markers = task.markers.copy(isImportant = !task.markers.isImportant)
+                        )
+                    }
+                }
+                .map { task ->
+                    State(
+                        type = task.type.format(task.dueDate),
+                        title = task.title,
+                        description = task.description,
+                        isUrgent = task.markers.isUrgent,
+                        isImportant = task.markers.isImportant
+                    )
+                }
         }
         .stateIn(
             scope = viewModelScope,
@@ -41,12 +63,29 @@ internal class EditTaskViewModel(
             )
         )
 
-    fun onTypeClick() {}
-    fun onTitleChange(title: String) {}
-    fun onDescriptionChange(description: String) {}
-    fun onUrgentClick() {}
-    fun onImportantClick() {}
-    fun onSave() {}
+    fun onTypeClick() {
+        //TODO
+    }
+
+    fun onTitleChange(title: String) {
+        actionChannel.trySend(Action.TitleChange(title))
+    }
+
+    fun onDescriptionChange(description: String) {
+        actionChannel.trySend(Action.DescriptionChange(description))
+    }
+
+    fun onUrgentClick() {
+        actionChannel.trySend(Action.UrgentClick)
+    }
+
+    fun onImportantClick() {
+        actionChannel.trySend(Action.ImportantClick)
+    }
+
+    fun onSave() {
+        //TODO
+    }
 
     //TODO: extract resources, reuse format logic
     private fun Task.Type.format(date: LocalDate? = null) = when (this) {
@@ -68,4 +107,10 @@ internal class EditTaskViewModel(
         Task.Type.MAYBE -> "MAYBE"
     }
 
+    private sealed class Action {
+        data class TitleChange(val title: String) : Action()
+        data class DescriptionChange(val description: String) : Action()
+        object UrgentClick : Action()
+        object ImportantClick : Action()
+    }
 }
