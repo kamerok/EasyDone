797f9f5a42d9b08f54301b155da5be8805497259
Maksim Dvoretskii <kamerok@gmail.com>
2021-11-11 16:40:43 +0300

    Connect real data

--- feature/home/src/main/java/easydone/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/easydone/feature/home/HomeViewModel.kt
@@ -1,26 +1,46 @@
 package easydone.feature.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import easydone.core.domain.DomainRepository
-import kotlinx.coroutines.flow.MutableStateFlow
+import easydone.core.domain.model.Task
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import java.time.LocalDate
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 
 
 internal class HomeViewModel(
-    private val repository: DomainRepository,
+    repository: DomainRepository,
     private val navigator: HomeNavigator
 ) : ViewModel() {
 
-    private fun task(i: Int = 0) = UiTask("id:$i", "Task $i", false, false, false)
-
-    val state: StateFlow<State> = MutableStateFlow(State(
-        inboxCount = 5,
-        todoTasks = (0..10).map { task(it) },
-        nextWaitingTask = task() to LocalDate.now().plusDays(10),
-        waitingCount = 10,
-        maybeTasks = (10..20).map { task(it) }
-    ))
+    val state: StateFlow<State> =
+        combine(
+            repository.getTasks(Task.Type.INBOX),
+            repository.getTasks(Task.Type.TO_DO),
+            repository.getTasks(Task.Type.WAITING),
+            repository.getTasks(Task.Type.MAYBE)
+        ) { inbox, todo, waiting, maybe ->
+            State(
+                inboxCount = inbox.size,
+                todoTasks = todo.map { it.toUiTask() },
+                nextWaitingTask = waiting.minByOrNull { it.dueDate!! }
+                    ?.let { it.toUiTask() to it.dueDate!! },
+                waitingCount = waiting.size,
+                maybeTasks = maybe.map { it.toUiTask() }
+            )
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = State(
+                inboxCount = 0,
+                todoTasks = emptyList(),
+                nextWaitingTask = null,
+                waitingCount = 0,
+                maybeTasks = emptyList()
+            )
+        )
 
     fun onAdd() {
         navigator.navigateToCreate()
@@ -37,5 +57,13 @@ internal class HomeViewModel(
     fun onSettings() {
         navigator.navigateToSettings()
     }
+
+    private fun Task.toUiTask() = UiTask(
+        id = id,
+        title = title,
+        hasDescription = description.isNotBlank(),
+        isUrgent = markers.isUrgent,
+        isImportant = markers.isImportant
+    )
 
 }
