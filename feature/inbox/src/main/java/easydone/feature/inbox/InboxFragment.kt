c022781754cb0f83e5848b26041b6007bbe4a24c
Maksim Dvoretskii <kamerok@gmail.com>
2024-08-18 13:25:39 +0200

    Add inbox foldable support

--- feature/inbox/src/main/java/easydone/feature/inbox/InboxFragment.kt
+++ feature/inbox/src/main/java/easydone/feature/inbox/InboxFragment.kt
@@ -6,9 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
 import easydone.core.domain.DomainRepository
 
 
@@ -17,21 +14,13 @@ class InboxFragment(
     private val navigator: InboxNavigator
 ) : Fragment() {
 
-    private val viewModel: InboxViewModel by viewModels(factoryProducer = {
-        viewModelFactory {
-            initializer {
-                InboxViewModel(repository, navigator)
-            }
-        }
-    })
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = ComposeView(requireContext()).apply {
         setContent {
-            InboxScreen(viewModel)
+            InboxRoute(repository, navigator)
         }
     }
 
--- feature/inbox/src/main/java/easydone/feature/inbox/InboxScreen.kt
+++ feature/inbox/src/main/java/easydone/feature/inbox/InboxScreen.kt
@@ -10,39 +10,75 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.window.core.layout.WindowWidthSizeClass.Companion.COMPACT
+import easydone.core.domain.DomainRepository
 import easydone.coreui.design.AppTheme
 import easydone.coreui.design.EasyDoneAppBar
+import easydone.coreui.design.FoldPreviews
 import easydone.coreui.design.TaskCard
+import easydone.coreui.design.UiTask
+import java.util.UUID
 
 
 @Composable
-internal fun InboxScreen(viewModel: InboxViewModel) {
+internal fun InboxRoute(
+    repository: DomainRepository,
+    navigator: InboxNavigator
+) {
+    val viewModel: InboxViewModel = viewModel {
+        InboxViewModel(repository, navigator)
+    }
+    val state by viewModel.state.collectAsState()
+    InboxScreen(
+        state = state,
+        onTaskClick = viewModel::onTaskClick
+    )
+}
+
+@Composable
+internal fun InboxScreen(
+    state: State,
+    onTaskClick: (UiTask) -> Unit
+) {
     AppTheme {
         FullscreenContent {
             Column {
+                val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+                val columns by remember(windowSizeClass) {
+                    derivedStateOf {
+                        if (windowSizeClass.windowWidthSizeClass == COMPACT) 1 else 2
+                    }
+                }
                 EasyDoneAppBar(modifier = Modifier.statusBarsPadding()) {
                     Text("Inbox")
                 }
-                val state = viewModel.state.collectAsState().value
-                LazyColumn(
+                LazyVerticalStaggeredGrid(
                     contentPadding = PaddingValues(16.dp),
-                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                    verticalItemSpacing = 16.dp,
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    columns = StaggeredGridCells.Fixed(columns),
                 ) {
                     items(state.tasks) { task ->
                         TaskCard(
                             task = task,
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .clickable { viewModel.onTaskClick(task) }
+                                .clickable { onTaskClick(task) }
                         )
                     }
                     item {
@@ -68,4 +104,16 @@ private fun FullscreenContent(
     ) {
         content()
     }
+}
+
+
+@FoldPreviews
+@Composable
+private fun InboxPreview() {
+    InboxScreen(
+        state = State((0..10).map {
+            UiTask(UUID.randomUUID().toString(), "Task $it", true, true, true)
+        }),
+        onTaskClick = {}
+    )
 }
